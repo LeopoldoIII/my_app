fa0e7f45d70d17a2151b35b2438bb183b0b71dec
LeopoldoIII <nathan.rx@gmail.com>
2022-04-10 00:48:46 -0500

    Images and text added

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'image_banner.dart';
 import 'text_section.dart';
-
-
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello'),
+        title: Text('Location Detail'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          TextSection(Colors.red),
-          TextSection(Colors.green),
-          TextSection(Colors.blue),
+          ImageBanner("/assets/images/kiyomizu-dera.jpg"),
+          TextSection("sumary1", "something1"),
+          TextSection("sumary2", "something2"),
+          TextSection("sumary3", "something3"),
         ],
       ),
     );
