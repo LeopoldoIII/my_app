223cfce2db5851f8b529241d6d1d0041b39b871d
LeopoldoIII <nathan.rx@gmail.com>
2022-04-12 23:59:40 -0500

    final commit

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -4,21 +4,25 @@ import 'text_section.dart';
 import '../../models/location.dart';
 
 class LocationDetail extends StatelessWidget {
+  final int _locationID;
+
+  LocationDetail(this._locationID);
+
   @override
   Widget build(BuildContext context) {
-    final locations = Location.fetchAll();
-    final location = locations.first;
+    
+    final location = Location.fetchByID(_locationID);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(location.name),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ImageBanner(location.imagePath),
-        ]..addAll(textSections(location))),
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ImageBanner(location.imagePath),
+          ]..addAll(textSections(location))),
     );
   }
 
